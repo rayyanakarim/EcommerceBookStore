b01d1eec8c5545177586e0d81a2b38dfbf0a5b87
rayyanakarim <rayyan3d@gmail.com>
2025-11-11 22:02:30 -0500

    Level 2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,100 @@
-
 import { HiMiniBars3BottomLeft } from "react-icons/hi2";
+import { IoSearch } from "react-icons/io5";
+import { FaUser } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
+import { LuShoppingCart } from "react-icons/lu";
 import { Link } from "react-router-dom";
+import avatar from "../assets/avatar.png";
+import { useState } from "react";
+
+const navigation = [
+  { name: "Dashboard", href: "/dashboard" },
+  { name: "Orders", href: "/order" },
+  { name: "Cart Page", href: "/cart" },
+  { name: "Check Out", href: "/checkout" },
+];
 
 const Navbar = () => {
+  const currentUser = true;
+  const [isDropdownOpen, setIsDropdownOpen] = useState(true);
+
   return (
-    <header className="max-w-screen-2xl max-auto px-4 py-6 font-primary">
-        <nav className="flex justify-between items-center">
-            {/** LeftNavigation */}
-            <div>
-                <Link to="/">
-                <HiMiniBars3BottomLeft />
-                </Link>
+    <header className="max-w-screen-2xl mx-auto px-4 py-6 font-primary">
+      <nav className="flex justify-between items-center">
+        {/** LeftNavigation */}
+
+        {/** LeftNavigation icon */}
+        <div className="flex items-center md:gap-16 gap-4">
+          <Link to="/">
+            <HiMiniBars3BottomLeft className="size-6" />
+          </Link>
+
+          {/** LeftNavigation searchbar */}
+          <div className="relative sm:w-72 w-40">
+            <IoSearch className="absolute inline-block left-3 inset-y-2" />
+
+            <input
+              type="text"
+              placeholder="Search books..."
+              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+            />
+          </div>
+        </div>
+
+        {/** RightNavigation */}
+        <div className="relative flex items-center md:space-x-3 space-x-2">
+          {currentUser ? (
+            <div className="flex items-center">
+              <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                <img
+                  className={`size-7 rounded-full ${
+                    currentUser ? "border-2 border-primary" : ""
+                  }`}
+                  src={avatar}
+                  alt="Profile"
+                />
+              </button>
+
+              {/* Dropdown Menu */}
+
+              {isDropdownOpen && (
+                <div>
+                  <ul>
+                    {navigation.map((item) => (
+                      <li key={item.name}>
+                        <Link
+                          to={item.href}
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                        >
+                          {item.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
+              )}
+            </div>
+          ) : (
+            <Link to="/login">
+              <FaUser className="size-6" />
+            </Link>
+          )}
 
-            {/** RightNavigation */}
-            <div>Right Navigation</div>
-        </nav>
+          <button className="hidden sm:inline-block ">
+            <FaRegHeart className="size-6" />
+          </button>
+
+          <Link
+            to="/cart"
+            className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-sm"
+          >
+            <LuShoppingCart className="size-6" />
+            <span className="text-sm font-semibold sm:ml-1">0</span>
+          </Link>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
