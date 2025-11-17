4a235c099ce1a04d2a8f6c607a4809a93f261f28
rayyanakarim <rayyan3d@gmail.com>
2025-11-17 15:49:14 -0500

    Stage 6

---
+++ frontend/src/pages/home/Recommened.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const Recommened = () => {
+  const [Books, setBooks] = useState([]);
+
+  useEffect(() => {
+    //fetch top selling books from backend api
+    fetch("Books.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold mb-6">Recommeneded for you</h2>
+
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {/* Books Grid */}
+
+        {filteredBooks.length > 0 &&
+          filteredBooks.map((book, index) => (
+            <SwiperSlide key={index}>
+              <BookCard book={book} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Recommened;
